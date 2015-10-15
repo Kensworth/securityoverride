a57652093b89752b6c2aaad84e284023a177cf12
Ken Zhang <kyz211@nyu.edu>
2015-10-15 15:30:30 -0400

    getting closer

--- regex-testing-3.php
+++ regex-testing-3.php
@@ -1,12 +1,8 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-
-//$regex = $_GET['highlight'];
-$string = preg_replace("/exit\(phpinfo\(\)\);/e", '$0', "exit(phpinfo());");
-echo $string;
-//echo "<br />";
-//echo $regex;
+	$string = "bluechill";
+	$regex = "exit\(phpinfo\(\)\);/e\", '$0', 'exit(phpinfo());');";
+	$string = preg_replace("/". $regex . "/", '$0 * $1', $string);
+	echo $string;
+	echo "<br />";
+	echo $regex;
 ?>
-
--- regex-testing-4.php
+++ regex-testing-4.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors',1);
 error_reporting(-1);
 
 //$regex = $_GET['highlight'];
-$string = preg_replace("/exit\(phpinfo\(\)\);\/\*/e", '$0', "exit(phpinfo());/*");
-/*echo $string;
+$string = preg_replace("/exit\(phpinfo\(\)\);/e", '$0', "exit(phpinfo());");
+echo $string;
 echo 
 ?>
 
