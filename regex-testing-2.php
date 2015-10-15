0029007e5a70c09465dc4f6f9483c7fa1e081af8
Ken Zhang <kyz211@nyu.edu>
2015-10-15 15:45:35 -0400

    changing course of action

--- regex-testing-2.php
+++ regex-testing-2.php
@@ -3,9 +3,11 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
+//how can you get /* to run?
+
 //$regex = $_GET['highlight'];
-$string = preg_replace("/exit\(phpinfo\(\)\);\/\*/e", '$0', "exit(phpinfo());/*");
-/*echo $string;
-echo 
+$string = preg_replace("/phpinfo\(\);/e", '$0', "phpinfo();");
+echo $string;
+//echo
 ?>
 
